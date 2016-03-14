2476f27867dc255872b3e47b44385eaf4b259bdc
grefen <ccbp2003@sohu.com>
2016-03-14 21:15:59 +0800

    add commit

--- verify/verify/rotate64bitboardchecksum/main.cpp
+++ verify/verify/rotate64bitboardchecksum/main.cpp
@@ -3,7 +3,7 @@
 #include "bitboardtest.h"
 int main()
 {
-	printf("rotate bitboard + knight imagic solution 64bit Bitboard V1.00\n");
+	printf("rotate bitboard + knight checksum solution 64bit Bitboard V1.00\n");
 
 	printf("the result is: depth 5 use 5.3 seconds\n");
 
